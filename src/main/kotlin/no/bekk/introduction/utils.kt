7091f06e9bed2876bf15aa4a99eeb6a953d4191d
Patrick Allport <patrick@bekk-mac-3124.shared>
2025-08-04 13:49:02 +0200

    La inn bootcampcoacher for 2025

--- src/main/kotlin/no/bekk/introduction/utils.kt
+++ src/main/kotlin/no/bekk/introduction/utils.kt
@@ -9,58 +9,88 @@ enum class Avdeling {
     TEKNOLOGI, DESIGN, BMC
 }
 
-val ingrid = BootcampCoach(
-    "Ingrid",
-    Avdeling.TEKNOLOGI,
-    3,
-)
-
-val kristian = BootcampCoach(
-    "Kristian",
+val august = BootcampCoach(
+    "August",
     Avdeling.TEKNOLOGI,
     4,
-)
-
-val ragnhild = BootcampCoach(
-    "Ragnhild",
-    Avdeling.TEKNOLOGI,
-    4,
-)
-
-val johan = BootcampCoach(
-    "Johan",
-    Avdeling.TEKNOLOGI,
-    8,
-)
-
-val ida = BootcampCoach(
-    "Ida",
-    Avdeling.TEKNOLOGI,
-    6,
-)
-
-val gabriel = BootcampCoach(
-    "Gabriel",
-    Avdeling.TEKNOLOGI,
-    2,
 )
 
 val aurora = BootcampCoach(
     "Aurora",
     Avdeling.DESIGN,
+    7,
+)
+
+val bendik = BootcampCoach(
+    "Bendik",
+    Avdeling.BMC,
+    4,
+)
+
+val frikk = BootcampCoach(
+    "Frikk",
+    Avdeling.TEKNOLOGI,
+    4,
+)
+
+val gabriel = BootcampCoach(
+    "Gabriel",
+    Avdeling.TEKNOLOGI,
+    4,
+)
+
+val hanna = BootcampCoach(
+    "Hanna",
+    Avdeling.TEKNOLOGI,
+    4,
+)
+
+val hansKristian = BootcampCoach(
+    "Hans Kristian",
+    Avdeling.TEKNOLOGI,
+    9,
+)
+
+val ingrid = BootcampCoach(
+    "Ingrid",
+    Avdeling.TEKNOLOGI,
     5,
 )
 
-val preben = BootcampCoach(
-    "Preben",
+val kristin = BootcampCoach(
+    "Kristin",
     Avdeling.DESIGN,
-    2,
+    3,
 )
 
 val morten = BootcampCoach(
     "Morten",
     Avdeling.BMC,
+    6,
+)
+
+val oleReidar = BootcampCoach(
+    "Ole Reidar",
+    Avdeling.TEKNOLOGI,
+    7,
+)
+
+val sara = BootcampCoach(
+    "Sara",
+    Avdeling.TEKNOLOGI,
+    3,
+)
+
+val sivert = BootcampCoach(
+    "Sivert",
+    Avdeling.TEKNOLOGI,
     4,
 )
 
-val coacher2025 = listOf<BootcampCoach>(ingrid, kristian, johan, ida, gabriel, ragnhild, aurora, preben, morten, ida)
+val vilde = BootcampCoach(
+    "Vilde",
+    Avdeling.DESIGN,
+    2,
+)
+
+val coacher2025 = listOf<BootcampCoach>(august, aurora, bendik, frikk, gabriel, hanna, hansKristian, ingrid, kristin, morten, oleReidar, sara, sivert, vilde)
