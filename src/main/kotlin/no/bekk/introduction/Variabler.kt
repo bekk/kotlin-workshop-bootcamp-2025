b16f3ad5429d61ca24d77fef1c586b86287a71c4
Patrick Allport <patrick@bekk-mac-3124.shared>
2025-08-05 14:02:45 +0200

    Fjernet løsningsforslag fra koden til Variabler

--- src/main/kotlin/no/bekk/introduction/Variabler.kt
+++ src/main/kotlin/no/bekk/introduction/Variabler.kt
@@ -1,10 +1,10 @@
 package no.bekk.introduction
 
 fun main() {
-    val tall1 = 1
+    var tall1 = 1
     println("Verdien til tall1 er: $tall1")
-    val tall2 = tall1 + 1
-    println("Verdien til tall1 pluss 1 er: $tall2")
-    val tall3 = tall2 / 2
-    println("Verdien til tall1 pluss 1 delt på to er: $tall3")
+    tall1 +=1
+    println("Verdien til tall1 pluss 1 er: $tall1")
+    tall1 /= 2
+    println("Verdien til tall1 pluss 1 delt på to er: $tall1")
 }
